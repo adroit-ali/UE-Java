8d71f102e5ef61a0aed39ce22219a3bf5cc0755f
Ali Shahjee <muhammadaliofficial77@gmail.com>
2024-12-06 12:05:11 +0500

    little change

--- src/Question1.java
+++ src/Question1.java
@@ -6,7 +6,7 @@ public class Question1 {
         String[] numerical = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         String[] StoreNumeral = new String[10];
         for (int i = 0; i < 10; i++) {
-            System.out.print("Enter "+i+" number: ");
+            System.out.print("Enter "+(i+1)+" number: ");
             int num = input.nextInt();
             if(1 <= num && num <= 10)
                 StoreNumeral[i] = numerical[num-1];
