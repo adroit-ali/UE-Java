cf20da50cdb6383573fd6821a8fd38b21d2faa68
Ali Shahjee <muhammadaliofficial77@gmail.com>
2024-12-06 11:58:04 +0500

    little change

--- src/Question1.java
+++ src/Question1.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        String[] numerical = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        String[] numerical = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         String[] StoreNumeral = new String[10];
         for (int i = 0; i < 10; i++) {
             System.out.print("Enter "+i+" number: ");
             int num = input.nextInt();
             if(1 <= num && num <= 10)
-                StoreNumeral[i] = numerical[num];
+                StoreNumeral[i] = numerical[num-1];
             else{
                 System.out.println("Invalid Number Try Again");
                 i--;
